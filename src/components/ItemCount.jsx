8c829fc6472b4f67fd3faa5d4de63c392a8c9b6f
Juan Garcia <garciaa.juan.ignacio@gmail.com>
2022-07-25 19:53:59 -0300

    3er desafio

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({initial, stock, onAdd}) => {
+    const [count, setCount] = useState(initial) 
+
+    const sumar = () => {
+        if(count < stock){
+        setCount(count + 1)
+        }
+    }
+const resta = () => {
+    if(count > initial){
+        setCount(count - 1)
+    }
+}
+
+    return (
+    <div>
+        <button onClick={resta}>-</button>
+        <span>{count}</span>
+        <button onClick={sumar}>+</button>
+        <button onClick={onAdd}>comprar</button>
+    </div>
+         
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,17 @@
+import ItemCount from "./ItemCount"
+
 const ItemListContainer = (props) => {
+    
+    const onAdd = () => {
+        console.log('lo agregaste al carrito mi rey')
+    }
+    
     return (
+        <div>
         <h3 className="greeting">{props.greeting}</h3>
-    )
+        <ItemCount initial={1} stock={15} onAdd={onAdd}/>
+        </div>
+        ) 
 }
 
 export default ItemListContainer;
